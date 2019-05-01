ebe84d77bbb77546c045862ef85f77bb757038e3
tjose <tijojosewc@gmail.com>
2019-05-01 11:40:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/yoctocustom/MainActivity.java
@@ -0,0 +1,211 @@
+package com.example.yoctocustom;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.yoctocustom.Yocto.YAPI;
+import com.example.yoctocustom.Yocto.YAPI_Exception;
+import com.example.yoctocustom.Yocto.YAccelerometer;
+import com.example.yoctocustom.Yocto.YCompass;
+import com.example.yoctocustom.Yocto.YGyro;
+import com.example.yoctocustom.Yocto.YModule;
+import com.example.yoctocustom.Yocto.YSensor;
+import com.example.yoctocustom.Yocto.YTilt;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    private ArrayAdapter<String> aa;
+    private String serial = "";
+    private Handler handler = null;
+
+    TextView statusTV;
+    String status="";
+
+    private static final String ACTION_USB_ATTACHED  = "android.hardware.usb.action.USB_DEVICE_ATTACHED";
+    private static final String ACTION_USB_DETACHED  = "android.hardware.usb.action.USB_DEVICE_DETACHED";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner my_spin =  findViewById(R.id.spinner1);
+        my_spin.setOnItemSelectedListener(this);
+        aa = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        my_spin.setAdapter(aa);
+        handler = new Handler();
+
+        statusTV = findViewById(R.id.status);
+
+        status = "\nONCREATE\n";
+        statusTV.setText(status);
+
+        BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if(action.equalsIgnoreCase(ACTION_USB_ATTACHED)){
+                    status = status+"\n called reviever";
+                    Toast.makeText(getApplicationContext(), "ATTACHED", Toast.LENGTH_SHORT).show();
+                    try {
+                        aa.clear();
+
+                        YAPI.EnableUSBHost(this);
+
+                        status = "\nENABLE HOST"+status;
+                        statusTV.setText(status);
+
+                        YAPI.RegisterHub("usb");
+
+                        status = "\nregistered HOST"+status;
+                        statusTV.setText(status);
+
+
+                        YModule module = YModule.FirstModule();
+                        while (module != null) {
+                            if (module.get_productName().equals("Yocto-3D-V2")) {
+                                String serial = module.get_serialNumber();
+                                aa.add(serial);
+                            }
+                            module = module.nextModule();
+                        }
+                    } catch (YAPI_Exception e) {
+                        e.printStackTrace();
+                    }
+                    aa.notifyDataSetChanged();
+                    handler.postDelayed(r, 500);
+                }
+
+                if(action.equalsIgnoreCase(ACTION_USB_DETACHED)){
+                    Toast.makeText(getApplicationContext(), "DETTACHED", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ACTION_USB_ATTACHED);
+        intentFilter.addAction(ACTION_USB_DETACHED);
+
+        registerReceiver(broadcastReceiver, intentFilter);
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        status = "\nON START"+status;
+        statusTV.setText(status);
+        try {
+            aa.clear();
+
+            YAPI.EnableUSBHost(this);
+
+            status = "\nENABLE HOST"+status;
+            statusTV.setText(status);
+
+            YAPI.RegisterHub("usb");
+
+            status = "\nregistered HOST"+status;
+            statusTV.setText(status);
+
+
+            YModule module = YModule.FirstModule();
+            while (module != null) {
+                if (module.get_productName().equals("Yocto-3D-V2")) {
+                    String serial = module.get_serialNumber();
+                    aa.add(serial);
+                }
+                module = module.nextModule();
+            }
+        } catch (YAPI_Exception e) {
+            e.printStackTrace();
+        }
+        aa.notifyDataSetChanged();
+        handler.postDelayed(r, 500);
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        handler.removeCallbacks(r);
+        YAPI.FreeAPI();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id)
+    {
+        serial = parent.getItemAtPosition(pos).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0)
+    {
+    }
+
+    final Runnable r = new Runnable()
+    {
+        public void run()
+        {
+            status= "\n runnable running"+status;
+            statusTV.setText(status);
+            if (serial != null) {
+                YSensor tilt1 = YTilt.FindTilt(serial + ".tilt1");
+                try {
+                    TextView view =  findViewById(R.id.tilt1field);
+                    view.setText(String.format("%.1f %s", tilt1.getCurrentValue(), tilt1.getUnit()));
+                } catch (YAPI_Exception e) {
+                    e.printStackTrace();
+                }
+                YTilt tilt2 = YTilt.FindTilt(serial + ".tilt2");
+                try {
+                    TextView view =  findViewById(R.id.tilt2field);
+                    view.setText(String.format("%.1f %s", tilt2.getCurrentValue(), tilt2.getUnit()));
+                } catch (YAPI_Exception e) {
+                    e.printStackTrace();
+                }
+                YCompass compass = YCompass.FindCompass(serial + ".compass");
+                try {
+                    TextView view = (TextView) findViewById(R.id.compassfield);
+                    view.setText(String.format("%.1f %s", compass.getCurrentValue(), compass.getUnit()));
+                } catch (YAPI_Exception e) {
+                    e.printStackTrace();
+                }
+                YAccelerometer accelerometer = YAccelerometer.FindAccelerometer(serial + ".accelerometer");
+                try {
+                    TextView view = (TextView) findViewById(R.id.accelfield);
+                    view.setText(String.format("%.1f %s", accelerometer.getCurrentValue(), accelerometer.getUnit()));
+                } catch (YAPI_Exception e) {
+                    e.printStackTrace();
+                }
+                YGyro gyro = YGyro.FindGyro(serial + ".gyro");
+                try {
+                    TextView view = (TextView) findViewById(R.id.gyrofield);
+                    view.setText(String.format("%.1f %s", gyro.getCurrentValue(), gyro.getUnit()));
+                } catch (YAPI_Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            handler.postDelayed(this, 200);
+        }
+    };
+
+}
---
+++ app/src/main/java/com/example/yoctocustom/Yocto/YWlanRecord.java
@@ -0,0 +1,120 @@
+/*********************************************************************
+ *
+ * $Id: YWlanRecord.java 34651 2019-03-15 17:21:54Z seb $
+ *
+ * YWlanRecord Class: Description of a wireless network detected
+ *
+ * - - - - - - - - - License information: - - - - - - - - -
+ *
+ *  Copyright (C) 2011 and beyond by Yoctopuce Sarl, Switzerland.
+ *
+ *  Yoctopuce Sarl (hereafter Licensor) grants to you a perpetual
+ *  non-exclusive license to use, modify, copy and integrate this
+ *  file into your software for the sole purpose of interfacing 
+ *  with Yoctopuce products. 
+ *
+ *  You may reproduce and distribute copies of this file in 
+ *  source or object form, as long as the sole purpose of this
+ *  code is to interface with Yoctopuce products. You must retain 
+ *  this notice in the distributed source file.
+ *
+ *  You should refer to Yoctopuce General Terms and Conditions
+ *  for additional information regarding your rights and 
+ *  obligations.
+ *
+ *  THE SOFTWARE AND DOCUMENTATION ARE PROVIDED 'AS IS' WITHOUT
+ *  WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING 
+ *  WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, FITNESS 
+ *  FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO
+ *  EVENT SHALL LICENSOR BE LIABLE FOR ANY INCIDENTAL, SPECIAL,
+ *  INDIRECT OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, 
+ *  COST OF PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR 
+ *  SERVICES, ANY CLAIMS BY THIRD PARTIES (INCLUDING BUT NOT 
+ *  LIMITED TO ANY DEFENSE THEREOF), ANY CLAIMS FOR INDEMNITY OR
+ *  CONTRIBUTION, OR OTHER SIMILAR COSTS, WHETHER ASSERTED ON THE
+ *  BASIS OF CONTRACT, TORT (INCLUDING NEGLIGENCE), BREACH OF
+ *  WARRANTY, OR OTHERWISE.
+ *
+ *********************************************************************/
+
+package com.example.yoctocustom.Yocto;
+
+//--- (generated code: YWlanRecord class start)
+/**
+ * YWlanRecord Class: Description of a wireless network
+ *
+ * YWlanRecord objects are used to describe a wireless network.
+ * These objects are  used in particular in conjunction with the
+ * YWireless class.
+ */
+@SuppressWarnings({"UnusedDeclaration", "UnusedAssignment"})
+public class YWlanRecord
+{
+//--- (end of generated code: YWlanRecord class start)
+    //--- (generated code: YWlanRecord definitions)
+    protected String _ssid;
+    protected int _channel = 0;
+    protected String _sec;
+    protected int _rssi = 0;
+
+    //--- (end of generated code: YWlanRecord definitions)
+
+    YWlanRecord(String json_str) throws YAPI_Exception
+    {
+        try {
+            YJSONObject json = new YJSONObject(json_str);
+            json.parse();
+            _ssid = json.getString("ssid");
+            _channel = json.getInt("channel");
+            _sec = json.getString("sec");
+            _rssi = json.getInt("rssi");
+        } catch (Exception e) {
+            throw new YAPI_Exception(YAPI.IO_ERROR, "invalid json struct for YWlanRecord");
+        }
+    }
+
+    //--- (generated code: YWlanRecord implementation)
+
+    /**
+     * Returns the name of the wireless network (SSID).
+     *
+     * @return a string with the name of the wireless network (SSID).
+     */
+    public String get_ssid()
+    {
+        return _ssid;
+    }
+
+    /**
+     * Returns the 802.11 channel.
+     *
+     * @return the 802.11 channel.
+     */
+    public int get_channel()
+    {
+        return _channel;
+    }
+
+    /**
+     * Returns the security algorithm used by the wireless network.
+     *
+     * @return a string with the security algorithm.
+     */
+    public String get_security()
+    {
+        return _sec;
+    }
+
+    /**
+     * Returns the quality of the wireless network link, in per cents.
+     *
+     * @return the quality of the wireless network link, in per cents.
+     */
+    public int get_linkQuality()
+    {
+        return _rssi;
+    }
+
+    //--- (end of generated code: YWlanRecord implementation)
+}
+
